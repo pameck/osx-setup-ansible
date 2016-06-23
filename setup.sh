10f6709e6bc126b1a875664daa0ccaaa9d7d2cbd
Pam Rucinque <pam@Pams-MacBook-Pro.local>
2016-06-23 22:08:19 +1000

    Main script, installs ansible and runs the playbook

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing pip"
+sudo easy_install pip
+echo "Installing pip done."
+
+echo "Upgrading setuptools"
+pip install --upgrade setuptools --user python
+echo "Upgrading setuptools done."
+
+echo "Installing ansible"
+sudo pip install ansible
+echo "Installing ansible done."
+
+ansible-playbook -i "localhost," -c local main.yml
